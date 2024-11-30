a9a70bcf641e01a63d6f17e99b0a7333ff7cd6c2
UmalKhan <i230021@isb.nu.edu.pk>
2024-12-01 00:57:06 +0500

    Dijkstra's algorithm

--- for now.cpp
+++ for now.cpp
@@ -20,11 +20,15 @@ struct Vertex {
     char id;
     Node* adj_list;
     Vertex* next;
+    int distance;
+    bool visited;
 
-    Vertex(char i, Node* a, Vertex* n) {
+    Vertex(char i) {
         id = i;
-        adj_list = a;
-        next = n;
+        adj_list = nullptr;
+        next = nullptr;
+        distance = INT_MAX;
+        visited = false;
     }
 };
 
@@ -37,8 +41,15 @@ struct Graph {
 
     void addVertex(char id) {
         if (!vertex_exists(id)) {
-            Vertex* new_vertex = new Vertex(id, nullptr, vertices);
-            vertices = new_vertex;
+            Vertex* new_vertex = new Vertex(id);
+            Vertex* temp = vertices;
+            if(!temp)
+                vertices = new_vertex;
+            else {
+                while(temp->next)
+                    temp = temp->next;
+                temp->next = new_vertex;
+            }
         }
     }
 
@@ -56,10 +67,10 @@ struct Graph {
         addVertex(src);
         addVertex(dest);
 
-        Vertex* srcVertex = find_vertex(src);
-        if (srcVertex) {
-            Node* new_node = new Node(dest, weight, srcVertex->adj_list);
-            srcVertex->adj_list = new_node;
+        Vertex* src_vertex = find_vertex(src);
+        if (src_vertex) {
+            Node* new_node = new Node(dest, weight, src_vertex->adj_list);
+            src_vertex->adj_list = new_node;
         }
     }
 
@@ -87,6 +98,57 @@ struct Graph {
             temp = temp->next;
         }
     }
+
+    void dijkstra(char source) {
+        Vertex* src_vertex = find_vertex(source);
+        if (!src_vertex) {
+            cout << "Source vertex not found!" << endl;
+            return;
+        }
+
+        src_vertex->distance = 0;
+
+        while (true) {
+            Vertex* current = nullptr;
+            Vertex* temp = vertices;
+            while (temp) {
+                if (!temp->visited && (current == nullptr || temp->distance < current->distance)) {
+                    current = temp;
+                }
+                temp = temp->next;
+            }
+
+            if (!current || current->distance == INT_MAX) 
+                break;
+
+            current->visited = true;
+
+            Node* adj = current->adj_list;
+            while (adj) {
+                Vertex* neighbor = find_vertex(adj->id);
+                if (neighbor && !neighbor->visited) {
+                    int new_dist = current->distance + adj->weight;
+                    if (new_dist < neighbor->distance) {
+                        neighbor->distance = new_dist;
+                    }
+                }
+                adj = adj->next;
+            }
+        }
+
+        cout << "Shortest distances from vertex " << source << ":\n";
+        Vertex* temp = vertices;
+        while (temp) {
+            cout << "To " << temp->id << ": ";
+            if (temp->distance == INT_MAX) {
+                cout << "INF\n";
+            } else {
+                cout << temp->distance << "\n";
+            }
+            temp = temp->next;
+        }
+    }
+
 
     ~Graph() {
         Vertex* temp = vertices;
@@ -128,6 +190,8 @@ int main() {
 
     read_file(g, "road_network.csv");
     g.print_graph();
+
+    g.dijkstra('X');
 
     return 0;
 }
